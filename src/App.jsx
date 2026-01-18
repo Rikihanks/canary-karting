9b791356d8968fed8d8713221cde929f8122283a
rikihanks <rilhi22@gmail.com>
2026-01-18 16:16:55 +0000

    feat: hide inscripcion xd

--- src/App.jsx
+++ src/App.jsx
@@ -38,7 +38,7 @@ function App() {
               } />
               <Route path="/inscripcion" element={
                 <FeatureGuard feature="inscripcion">
-                  <Inscripcion />
+                  {/*<Inscripcion />*/}
                 </FeatureGuard>
               } />
 
